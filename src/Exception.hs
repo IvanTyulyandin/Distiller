95fdf93a5bda4a953bc17d89708e674e25f81294
Ivan Tyulyandin <ivan.tyulyandin@gmail.com>
2019-11-13 16:50:16 +0300

    Apply haskell-formatter tool

--- src/Exception.hs
+++ src/Exception.hs
@@ -1,24 +1,27 @@
 module Exception where
 
-import Control.Monad (ap,liftM)
+import Control.Monad (ap, liftM)
 
-data Exception a b = Exn a | NoExn b deriving Show
+data Exception a b = Exn a
+                   | NoExn b
+                       deriving Show
 
 instance Functor (Exception a) where
-    fmap = liftM
+        fmap = liftM
 
 instance Applicative (Exception a) where
-    pure x = NoExn x
-    (<*>) = ap
+        pure x = NoExn x
+        (<*>) = ap
 
 instance Monad (Exception a) where
-    (>>=) (Exn d) f = (Exn d)
-    (>>=) (NoExn a) f = f a
+        (>>=) (Exn d) f = (Exn d)
+        (>>=) (NoExn a) f = f a
 
 handle :: Exception a b -> (a -> Exception a b) -> Exception a b
-handle x f = case x of
-               Exn c -> f c
-               NoExn a -> NoExn a
+handle x f
+  = case x of
+        Exn c -> f c
+        NoExn a -> NoExn a
 
 throw :: a -> Exception a b
 throw x = Exn x
